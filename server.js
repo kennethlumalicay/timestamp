2ea9c5ce4b65893bef2b052ccde2a98deafb235d
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-20 23:32:40 -0400

    fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var app = express();
 var url = require("url");
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 app.get("/*", function(req, res) {
 	var queryUrl = url.parse(req.url, true);
 	var arr = queryUrl.pathname.substring(1).replace(/%20|\,/g , " ").split(/ +/);
-	console.log(arr.toString() + " length " + arr.length); // month,date,,year length 4
+	console.log(arr.toString() + " length " + arr.length);
 	if(arr.toString() === "") {
-		res.send("get me some date!\nFormat: month date year");
+		// home page
+		res.send("get me some date! <br> Format: month date year");
 	} else {
 		var obj = {
 			unix: getUnix(arr),
@@ -21,8 +23,7 @@ function getUnix(arr) {
 		var d = getNatural(arr);
 		return new Date(d).getTime()/1000; // ms to s
 	} else if(arr.toString().match(/\D/)) {
-		console.log("non digit");
-		return null;
+		return null; // non digit
 	} else {
 		return new Date(parseInt(arr)).getTime();
 	}
@@ -31,59 +32,18 @@ function getNatural(arr) {
 	if(arr.length === 3) {
 		var d = new Date(arr[0] + " " + arr[1] + ", " + arr[2]);
 		if(d) {
-			return monthString(d.getMonth()).substring(0,1).toUpperCase() + monthString(d.getMonth()).substring(1) + " " + d.getDate() + ", " + d.getFullYear();
+			return months[d.getMonth()].substring(0,1).toUpperCase() + months[d.getMonth()].substring(1) + " " + d.getDate() + ", " + d.getFullYear();
 		}
 		return d;
 	} else if(arr.toString().match(/\D/)) {
-		console.log("non digit");
-		return null;
+		return null; // non digit
 	} else {
 		var d = new Date(parseInt(arr)*1000); // s to ms
 		if(d) {
-			return monthString(d.getMonth()) + " " +
+			return months[d.getMonth()] + " " +
 				d.getDate() + ", " + d.getFullYear();
 		}
 		return d;
-	}
-}
-function monthString(month) {
-	switch (month) {
-		case 0:
-			return "January";
-			break;
-		case 1:
-			return "February";
-			break;
-		case 2:
-			return "March";
-			break;
-		case 3:
-			return "April";
-			break;
-		case 4:
-			return "May";
-			break;
-		case 5:
-			return "June";
-			break;
-		case 6:
-			return "July";
-			break;
-		case 7:
-			return "August";
-			break;
-		case 8:
-			return "September";
-			break;
-		case 9:
-			return "October";
-			break;
-		case 10:
-			return "November";
-			break;
-		case 11:
-			return "December";
-			break;
 	}
 }
 app.listen(process.env.PORT || 3000);
